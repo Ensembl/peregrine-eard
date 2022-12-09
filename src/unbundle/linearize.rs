830a52586e24d74e452808a992ac765cd9157b16
Dan Sheppard <dan@ebi.ac.uk>
2022-12-09 18:14:26 +0000

    Linearize functions.

--- src/unbundle/linearize.rs
+++ src/unbundle/linearize.rs
@@ -1,5 +1,5 @@
 use std::{collections::{HashMap, HashSet}, sync::Arc};
-use crate::{buildtree::{BuildTree, BTStatement, BTStatementValue, BTLValue, BTProcCall, BTExpression, BTRegisterType}, parsetree::at, model::{Variable, OrBundleRepeater, LinearStatement, LinearStatementValue, OrBundle}};
+use crate::{buildtree::{BuildTree, BTStatement, BTStatementValue, BTLValue, BTProcCall, BTExpression, BTRegisterType, BTFuncProcDefinition}, parsetree::at, model::{Variable, OrBundleRepeater, LinearStatement, LinearStatementValue, OrBundle, TypedArgument}};
 use super::unbundleaux::Position;
 
 type Bundles = HashMap<(Vec<usize>,Position),Vec<String>>;
@@ -68,15 +68,9 @@ impl<'a> Linearize<'a> {
         ).unwrap_or("*anon*".to_string())
     }
 
-    // TODO catch unknown vairables in ver_variable when expected known
-    // TODO type checking!
-    fn proc_statements(&mut self, proc: &BTProcCall<OrBundleRepeater<BTLValue>>, arg_regs: &[usize], ret_regs: &[usize]) -> Result<(),String> {
-        self.var_registers.push(HashMap::new());
-        /* bind args */
-        println!("\nproc_statements[{:?}]({:?},{:?})\n",proc.proc_index,arg_regs,ret_regs);
-        let defn = self.tree.get_procedure(proc)?.expect("proc_statements called on None");
+    fn callee_args(&mut self, defn_args: &[OrBundle<TypedArgument>], arg_regs: &[usize]) -> Result<(),String> {
         let mut regs = arg_regs.iter();
-        for (i,arg) in defn.args.iter().enumerate() {
+        for (i,arg) in defn_args.iter().enumerate() {
             match arg {
                 OrBundle::Normal(arg) => {
                     let var_reg = self.var_register(&Variable { name: arg.id.clone(), prefix: None });
@@ -93,25 +87,25 @@ impl<'a> Linearize<'a> {
                 }
             }
         }
-        /* run statements */
-        for stmt in &defn.block {
-            self.statement(stmt)?;
-        }
-        /* evaluate return expr */
-        let mut regs = ret_regs.iter();
-        for (i,ret) in defn.ret.iter().enumerate() {
+        Ok(())
+    }
+
+    fn callee_rets(&mut self, defn_ret: &[OrBundle<BTExpression>]) -> Result<Vec<usize>,String> {
+        let mut regs = vec![];
+        for (i,ret) in defn_ret.iter().enumerate() {
             match ret {
                 OrBundle::Normal(expr) => {
                     match expr {
                         BTExpression::Constant(c) => {
-                            self.add(LinearStatementValue::Constant(*regs.next().unwrap(),c.clone()));
+                            let reg = self.anon_register();
+                            regs.push(reg);
+                            self.add(LinearStatementValue::Constant(reg,c.clone()));
                         },
                         BTExpression::Variable(variable) => {
-                            let src = self.var_register(variable);
-                            self.add(LinearStatementValue::Copy(*regs.next().unwrap(),src));
+                            regs.push(self.var_register(variable));
                         },
                         BTExpression::RegisterValue(src,BTRegisterType::Normal) => {
-                            self.add(LinearStatementValue::Copy(*regs.next().unwrap(),*src));
+                            regs.push(*src);
                         },
                         BTExpression::RegisterValue(src,BTRegisterType::Bundle) => {
                             let key = &(self.call_stack.clone(),Position::Return(i));
@@ -119,10 +113,17 @@ impl<'a> Linearize<'a> {
                                 .ok_or_else(|| "cannot find bundle/F")?;
                             for name in bundle {
                                 let var_reg = self.bt_register(*src,Some(name));
-                                self.add(LinearStatementValue::Copy(*regs.next().unwrap(),var_reg));
+                                regs.push(var_reg);
                             }        
                         },
-                        BTExpression::Function(_) => todo!(),
+                        BTExpression::Function(func) => {
+                            self.call_stack.push(func.call_index);
+                            let func_args = self.caller_args(&func.args)?;
+                            let defn = self.tree.get_function(func)?;
+                            let mut ret_regs = self.callee(&defn,&func_args)?;
+                            regs.append(&mut ret_regs);
+                            self.call_stack.pop();
+                        }
                     }
                 },
                 OrBundle::Bundle(prefix) => {
@@ -131,20 +132,30 @@ impl<'a> Linearize<'a> {
                         .ok_or_else(|| "cannot find bundle/G")?;
                     for name in bundle {
                         let var_reg = self.var_register(&Variable { name: name.clone(), prefix: Some(prefix.clone()) });
-                        self.add(LinearStatementValue::Copy(*regs.next().unwrap(),var_reg));
+                        regs.push(var_reg);
                     }
                 }
             }
-        }
-        self.var_registers.pop();
-        Ok(())
+        } 
+        Ok(regs)
     }
 
-    fn procedure(&mut self, proc: &BTProcCall<OrBundleRepeater<BTLValue>>) -> Result<(),String> {
-        /* make registers for all the arguments */
-        self.call_stack.push(proc.call_index);
+    // TODO catch unknown vairables in ver_variable when expected known
+    // TODO type checking!
+    fn callee(&mut self, defn: &BTFuncProcDefinition, arg_regs: &[usize]) -> Result<Vec<usize>,String> {
+        self.var_registers.push(HashMap::new());
+        self.callee_args(&defn.args,arg_regs)?;
+        for stmt in &defn.block {
+            self.statement(stmt)?;
+        }
+        let callee_rets = self.callee_rets(&defn.ret)?;
+        self.var_registers.pop();
+        Ok(callee_rets)
+    }
+
+    fn caller_args(&mut self, args: &[OrBundleRepeater<BTExpression>]) -> Result<Vec<usize>,String> {
         let mut arg_regs = vec![];
-        for (i,arg) in proc.args.iter().enumerate() {
+        for (i,arg) in args.iter().enumerate() {
             match arg {
                 OrBundleRepeater::Normal(expr) => {
                     match expr {
@@ -176,7 +187,14 @@ impl<'a> Linearize<'a> {
                                 arg_regs.push(arg_reg);
                             }                                    
                         },
-                        BTExpression::Function(_) => todo!(),
+                        BTExpression::Function(func) => {
+                            self.call_stack.push(func.call_index);
+                            let func_args = self.caller_args(&func.args)?;
+                            let defn = self.tree.get_function(func)?;
+                            let mut ret_regs = self.callee(&defn,&func_args)?;
+                            arg_regs.append(&mut ret_regs);
+                            self.call_stack.pop();
+                        }
                     }
                 },
                 OrBundleRepeater::Bundle(bundle_name) =>  {
@@ -196,22 +214,28 @@ impl<'a> Linearize<'a> {
                 OrBundleRepeater::Repeater(_) => todo!(),
             }
         }
-        /* make registers for all the targets */
-        let mut ret_regs = vec![];
-        for (i,ret) in proc.rets.as_ref().unwrap_or(&vec![]).iter().enumerate() {
+        Ok(arg_regs)
+    }
+
+    fn callee_to_caller(&mut self, rets: &[OrBundleRepeater<BTLValue>], callee_rets: &[usize]) -> Result<(),String> {
+        let mut src = callee_rets.iter();
+        for (i,ret) in rets.iter().enumerate() {
             match ret {
                 OrBundleRepeater::Normal(BTLValue::Variable(variable)) => {
-                    ret_regs.push(self.var_register(&variable));
+                    let dst = self.var_register(&variable);
+                    self.add(LinearStatementValue::Copy(dst,*src.next().unwrap()));
                 },
                 OrBundleRepeater::Normal(BTLValue::Register(register,BTRegisterType::Normal)) => {
-                    ret_regs.push(self.bt_register(*register,None));
+                    let dst = self.bt_register(*register,None);
+                    self.add(LinearStatementValue::Copy(dst,*src.next().unwrap()));
                 },
                 OrBundleRepeater::Normal(BTLValue::Register(bt_register,BTRegisterType::Bundle)) => {
                     let key = &(self.call_stack.clone(),Position::Return(i));
                     let bundle = self.bundles.get(&key)
                         .ok_or_else(|| "cannot find bundle/D")?;
                     for bundle_arg in bundle {
-                        ret_regs.push(self.bt_register(*bt_register,Some(bundle_arg)));
+                        let dst = self.bt_register(*bt_register,Some(bundle_arg));
+                        self.add(LinearStatementValue::Copy(dst,*src.next().unwrap()));
                     }    
                 },
                 OrBundleRepeater::Bundle(bundle_name) => {
@@ -223,19 +247,23 @@ impl<'a> Linearize<'a> {
                             prefix: Some(bundle_name.to_string()),
                             name: bundle_arg.to_string()
                         });
-                        ret_regs.push(variable);
+                        self.add(LinearStatementValue::Copy(variable,*src.next().unwrap()));
                     }
                 },
                 OrBundleRepeater::Repeater(_) => todo!(),
             };
         }
-        /**/
-        if let Some(proc_index) = &proc.proc_index {
-            self.proc_statements(proc,&arg_regs,&ret_regs)?;
+        Ok(())
+    }
+
+    fn procedure(&mut self, proc: &BTProcCall<OrBundleRepeater<BTLValue>>) -> Result<(),String> {
+        self.call_stack.push(proc.call_index);
+        let arg_regs = self.caller_args(&proc.args)?;
+        if let Some(defn) = self.tree.get_procedure(proc)? {
+            let callee_rets = self.callee(defn,&arg_regs)?;
+            self.callee_to_caller(proc.rets.as_ref().unwrap_or(&vec![]),&callee_rets)?;
         } else {
-            for (ret,arg) in ret_regs.iter().zip(arg_regs.iter()) {
-                self.add(LinearStatementValue::Copy(*ret,*arg));
-            }
+            self.callee_to_caller(proc.rets.as_ref().unwrap_or(&vec![]),&arg_regs)?;
         }
         self.call_stack.pop();
         Ok(())

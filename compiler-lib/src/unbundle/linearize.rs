d10081cc24443ba35532a2d8c256321ab9cd2b9f
Dan Sheppard <dan@ebi.ac.uk>
2023-01-15 02:24:01 +0000

    Enable empty bundles. Why were they ever disabled?

--- compiler-lib/src/unbundle/linearize.rs
+++ compiler-lib/src/unbundle/linearize.rs
@@ -183,13 +183,12 @@ impl<'a> Linearize<'a> {
                     }
                 },
                 OrBundle::Bundle(prefix) => {
-                    if !self.var_registers.check_used(prefix) {
-                        return Err(format!("empty bundle return '{}'",prefix));
-                    }
-                    let bundle = self.bundles.get(&self.call_stack,&Position::Return(i))?;
-                    for name in bundle {
-                        let var_reg = self.var_registers.get(&Variable { name: name.clone(), prefix: Some(prefix.clone()) })?;
-                        regs.push(var_reg);
+                    if self.var_registers.check_used(prefix) {
+                        let bundle = self.bundles.get(&self.call_stack,&Position::Return(i))?;
+                        for name in bundle {
+                            let var_reg = self.var_registers.get(&Variable { name: name.clone(), prefix: Some(prefix.clone()) })?;
+                            regs.push(var_reg);
+                        }
                     }
                 }
             }
@@ -292,18 +291,17 @@ impl<'a> Linearize<'a> {
                     }
                 },
                 OrBundleRepeater::Bundle(bundle_name) =>  {
-                    if !self.var_registers.check_used(bundle_name) {
-                        return Err(format!("empty bundle argument '{}'",bundle_name));
-                    }
-                    let bundle = self.bundles.get(&self.call_stack,&Position::Arg(i))?;
-                    for bundle_arg in bundle {
-                        let arg_reg = self.allocator.next_register();
-                        let variable = self.var_registers.get(&Variable {
-                            prefix: Some(bundle_name.to_string()),
-                            name: bundle_arg.to_string()
-                        })?;
-                        self.add(LinearStatementValue::Copy(arg_reg,variable));
-                        arg_regs.push(arg_reg);
+                    if self.var_registers.check_used(bundle_name) {
+                        let bundle = self.bundles.get(&self.call_stack,&Position::Arg(i))?;
+                        for bundle_arg in bundle {
+                            let arg_reg = self.allocator.next_register();
+                            let variable = self.var_registers.get(&Variable {
+                                prefix: Some(bundle_name.to_string()),
+                                name: bundle_arg.to_string()
+                            })?;
+                            self.add(LinearStatementValue::Copy(arg_reg,variable));
+                            arg_regs.push(arg_reg);
+                        }
                     }
                 },
                 OrBundleRepeater::Repeater(_) => { panic!("Should be no repeaters here/A") },
@@ -384,14 +382,13 @@ impl<'a> Linearize<'a> {
     fn procedure(&mut self, proc: &BTProcCall<OrBundleRepeater<BTLValue>>) -> Result<(),String> {
         self.call_stack.push(proc.call_index);
         if let Some((left_prefix,right_prefix)) = find_repeater_arguments(proc)? {
-            if !self.var_registers.check_used(&right_prefix) {
-                return Err(format!("empty bundle used in repeater '{}'",right_prefix));
-            }
-            for name in self.bundles.get(&self.call_stack,&Position::Repeater)? {
-                let left = Variable { prefix: Some(left_prefix.clone()), name: name.to_string() };
-                let right = Variable { prefix: Some(right_prefix.clone()), name: name.to_string() };
-                let call = rewrite_repeater(proc,&left,&right)?;
-                self.non_repeater_procedure(&call)?;
+            if self.var_registers.check_used(&right_prefix) {
+                for name in self.bundles.get(&self.call_stack,&Position::Repeater)? {
+                    let left = Variable { prefix: Some(left_prefix.clone()), name: name.to_string() };
+                    let right = Variable { prefix: Some(right_prefix.clone()), name: name.to_string() };
+                    let call = rewrite_repeater(proc,&left,&right)?;
+                    self.non_repeater_procedure(&call)?;
+                }
             }
         } else {
             self.non_repeater_procedure(&proc)?;
